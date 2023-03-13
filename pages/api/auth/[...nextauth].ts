22c392bf05158e96d4cdf3f1644581ed6a894d07
sappkevin <109696421+sappkevin@users.noreply.github.com>
2023-03-13 15:31:40 -0500

    Add only Google Auth
    Keep only Google Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
+//import FacebookProvider from "next-auth/providers/facebook"
+//import GithubProvider from "next-auth/providers/github"
+//import TwitterProvider from "next-auth/providers/twitter"
+//import Auth0Provider from "next-auth/providers/auth0"
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
@@ -29,7 +29,11 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     */
-    FacebookProvider({
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    /*FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
     }),
@@ -37,10 +41,7 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
+    
     TwitterProvider({
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
@@ -49,7 +50,7 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.AUTH0_ID,
       clientSecret: process.env.AUTH0_SECRET,
       issuer: process.env.AUTH0_ISSUER,
-    }),
+    }),*/
   ],
   theme: {
     colorScheme: "light",
